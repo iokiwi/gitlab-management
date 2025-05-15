7ad6c6256474942d3328623014ac2ebb812ef7f2
Simon Merrick <s.m3rrick@gmail.com>
2025-05-15 14:35:59 +1200

    Support recursive management of groups and selective management of fields

--- gitlab_config/main.py
+++ gitlab_config/main.py
@@ -9,88 +9,165 @@ from prettytable import PrettyTable
 from dotenv import load_dotenv
 from gitlab.v4.objects.projects import Project
 
+from typing import Dict
+from pathlib import Path
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
+def read_mr_template(template_file: Path = Path("templates/merge_requests_template.txt")) -> str:
+    """Read the merge request template from a file in the current directory."""
 
-def extract_project_details(project: Project, fix: bool = False):
+    try:
+        with open(template_file, 'r') as file:
+            template_content = file.read()
+            return template_content
+    except FileNotFoundError:
+        logger.warning(f"Template file '{template_file}' not found. No template will be set.")
+        return None
+    except Exception as e:
+        logger.error(f"Error reading template file: {e}")
+        return None
+
+# Global lmao
+managed_fields = {
+    "squash_option": "default_on",
+    "prevent_secrets": True,
+    "remove_source_branch_after_merge": True,
+    "merge_method": "merge",
+    "merge_access_levels": "Developers + Maintainers",
+    "only_allow_merge_if_pipeline_succeeds": True,
+    "merge_requests_template": read_mr_template(),
+}
+
+def manage_project_settings(project: Project, fix: bool = False, depth=0) -> Dict:
     # Fetch the project by ID to get detailed information, including the default branch
-    print(f"Checking {project.name}... ", end="")
+    print("   " * depth, f"Checking {project.name}... ")
+
+    project_changes = []
+    push_rule_changes = []
 
     protected_branches = project.protectedbranches.list()
     push_rules = project.pushrules.get()
 
-    for branch in protected_branches:
-        if branch.name == project.default_branch:
-            default_protected_branch = project.branches.get(branch.name)
+    output_fields = {
+        "project": project.name,
+        "default_branch": project.default_branch,
+        "protected branches": ", ".join([b.name for b in protected_branches]),
+    }
 
-    merge_access_levels_default_branch = []
-    for level in branch.merge_access_levels:
-        merge_access_levels_default_branch.append(level["access_level_description"])
+    for field, expected in managed_fields.items():
 
-    if fix:
-        project_changes = []
-        push_rule_changes = []
+        if field == "remove_source_branch_after_merge":
+            if fix:
+                if project.remove_source_branch_after_merge is not expected:
+                    project_changes.append(
+                        f"remove_source_branch_after_merge: {project.remove_source_branch_after_merge} -> {expected}"
+                    )
+                    project.remove_source_branch_after_merge = expected
+            output_fields[field] = project.remove_source_branch_after_merge
 
-        # Ensure the default protected branch does not allow anyone to push
-        # and allows developers and maintainers to merge.
-        try:
-            if (
-                len(merge_access_levels_default_branch) != 1
-                or merge_access_levels_default_branch[0] != "Developers + Maintainers"
-            ):
-                project_changes.append(
-                    f"Merge rule: {project.squash_option} -> default_on"
-                )
-                project.protectedbranches.delete(default_protected_branch.name)
-                project.protectedbranches.create(
-                    {
-                        "name": default_protected_branch.name,
-                        "merge_access_level": gitlab.const.AccessLevel.DEVELOPER,
-                        "push_access_level": gitlab.const.AccessLevel.NO_ACCESS,
-                        "allow_force_push": False,
-                    }
-                )
-        except TypeError as e:
-            logging.exception(e)
-
-        # Delete the branch after mergeing
-        if project.remove_source_branch_after_merge is not True:
-            project.append(
-                f"remove_source_branch_after_merge: {project.remove_source_branch_after_merge} -> True"
-            )
-            project.remove_source_branch_after_merge = True
+        if field == "only_allow_merge_if_pipeline_succeeds":
+            if fix:
+                if project.only_allow_merge_if_pipeline_succeeds is not expected:
+                    project_changes.append(
+                        f"only_allow_merge_if_pipeline_succeeds: {project.only_allow_merge_if_pipeline_succeeds} -> {expected}"
+                    )
+                    project.only_allow_merge_if_pipeline_succeeds = expected
+            output_fields[field] = project.only_allow_merge_if_pipeline_succeeds
 
         # Set merge method to FF for projects with a singular 'main' branch only.
         # https://docs.gitlab.com/ee/user/project/merge_requests/methods/#fast-forward-merge
-        if project.merge_method != "ff":
-            # We only do FF if we have a singular 'main' branch
-            if project.default_branch == "main":
-                project_changes.append(f"merge_method: {project.default_branch} -> ff")
-                project.merge_method = "ff"
+        if field == "merge_method":
+            if fix:
+                managed_fields["merge_method"]
+                if project.merge_method != "ff":
+                    # We only do FF if we have a singular 'main' branch
+                    if project.default_branch == "main":
+                        project_changes.append(f"merge_method: {project.default_branch} -> ff")
+                        project.merge_method = "ff"
+            output_fields[field] = project.merge_method
+
+        if field == "merge_access_levels":
+            merge_access_levels_default_branch = []
+            for branch in protected_branches:
+                if branch.name == project.default_branch:
+                    default_protected_branch = project.branches.get(branch.name)
+
+                    for level in branch.merge_access_levels:
+                        merge_access_levels_default_branch.append(level["access_level_description"])
+
+            output_fields[field] = ",".join(merge_access_levels_default_branch)
+
+            if fix:
+                try:
+                    if (
+                        len(merge_access_levels_default_branch) != 1
+                        or merge_access_levels_default_branch[0] != "Developers + Maintainers"
+                    ):
+                        project_changes.append(
+                            f"Merge rule: {project.squash_option} -> default_on"
+                        )
+                        project.protectedbranches.delete(default_protected_branch.name)
+                        project.protectedbranches.create(
+                            {
+                                "name": default_protected_branch.name,
+                                "merge_access_level": gitlab.const.AccessLevel.DEVELOPER,
+                                "push_access_level": gitlab.const.AccessLevel.NO_ACCESS,
+                                "allow_force_push": False,
+                            }
+                        )
+                except TypeError as e:
+                    logging.exception(e)
 
         # Enable squash and merge by default (can be opted out of)
         # https://docs.gitlab.com/ee/user/project/merge_requests/squash_and_merge.html#set-default-squash-options-for-a-merge-request
-        if project.squash_option != "default_on":
-            project_changes.append(
-                f"squash_options: {project.squash_option} -> default_on"
-            )
-            project.squash_option = "default_on"
+        if field == "squash_option":
+            if fix:
+                if project.squash_option != expected:
+                    project_changes.append(
+                        f"squash_options: {project.squash_option} -> {expected}"
+                    )
+                    project.squash_option = expected
+
+            output_fields[field] = project.squash_option
+
+        if field == "merge_requests_template":
+            # Set merge request template if available
+
+            if fix:
+                # Check if the merge request template is different from the current one
+                if project.merge_requests_template != expected:
+                    project_changes.append("merge_requests_template: updated from template file")
+                    project.merge_requests_template = expected
+
+            # Curate the output so it doesn't break table
+            if project.merge_requests_template is None:
+                output_fields[field] = project.merge_requests_template
+            elif project.merge_requests_template != expected:
+                output_fields[field] = "Unexpected Template"
+            else:
+                output_fields[field] = "Template matches configuration"
 
         # Prevent pushing secret files
         # https://docs.gitlab.com/ee/user/project/repository/push_rules.html#prevent-pushing-secrets-to-the-repository
-        if push_rules.prevent_secrets is not True:
-            push_rule_changes.append(
-                f"prevent_secrets: {push_rules.prevent_secrets} -> True"
-            )
-            push_rules.prevent_secrets = True
+        if field == "prevent_secrets":
+            if fix:
+                if push_rules.prevent_secrets is not expected:
+                    push_rule_changes.append(
+                        f"prevent_secrets: {push_rules.prevent_secrets} -> {expected}"
+                    )
+                    push_rules.prevent_secrets = expected
+            output_fields[field] = push_rules.prevent_secrets
 
-        # Push Rules to manage
+        # TODO: Check status of 'Enable Merged Results'
+
+        # Push Rules we might want to manage in the future
         # TODO: Reject unverified users
         # TODO: Reject inconsistent username
         # TODO: Check whether the commit autor is a GitLab user
 
+    if fix:
         if push_rule_changes:
             push_rules.save()
 
@@ -104,73 +181,83 @@ def extract_project_details(project: Project, fix: bool = False):
             for change in push_rule_changes + project_changes:
                 print(f"\t{change}")
 
-    # TODO: Check status of 'Enable Merged Results'
 
-    return [
-        project.name,
-        project.default_branch,
-        project.squash_option,
-        push_rules.prevent_secrets,
-        project.remove_source_branch_after_merge,
-        project.only_allow_merge_if_pipeline_succeeds,
-        project.merge_method,
-        ", ".join([b.name for b in protected_branches]),
-        ",".join(merge_access_levels_default_branch),
-    ]
+    return output_fields
 
+
+def manage_projects_in_group(
+    gl, group_id: str, fix=False, limit=None, count=0, recurse=False, depth=0):
+
+    rows = []
+
+    group = gl.groups.get(group_id)
+    print("  " * depth, "📁" ,f"Getting projects for {group.name} ({group.id})")
+    group_projects = group.projects.list(get_all=True, archived=False)
+
+    for group_project in group_projects:
+        project = gl.projects.get(group_project.id)
+        try:
+            rows.append(manage_project_settings(project, fix, depth=depth+1))
+        except Exception as e:
+            logging.exception(e)
+
+        count += 1
+        if limit is not None and count == limit:
+            print(f"Limit of {limit} reached. Exiting")
+            break
+
+    if recurse:
+        subgroups = group.subgroups.list(all=True, archived=False)
+        for subgroup in subgroups:
+            if limit is not None and count >= limit:
+                break
+
+            subrows, count = manage_projects_in_group(
+                gl,
+                subgroup.id,
+                fix=fix,
+                limit=limit,
+                count=count,
+                recurse=recurse,
+                depth=depth + 1,
+            )
+
+            rows += subrows
+
+    return (rows, count)
 
 def main():
     parser = ArgumentParser()
+    parser.add_argument("--fix", action="store_true", help="Script will not make changes unless --fix is passed")
+    parser.add_argument("--limit", type=int, help="Stop after doing <n> projects. Helpful for testing")
+    parser.add_argument("-r", "--recurse-subprojects", default=False, action="store_true")
     # TODO: Add argument to specify a project by name and / or ID
-    parser.add_argument("--fix", action="store_true")
-    parser.add_argument("--limit", type=int)
-
     args = parser.parse_args()
 
     load_dotenv()
+
     # GitLab private token
     GITLAB_URL = "https://gitlab.com"
     PRIVATE_TOKEN = os.environ.get("GITLAB_TOKEN")
-    GROUP_ID = os.environ.get("GROUP_ID")
+    GROUP_ID = os.environ.get("GITLAB_GROUP_ID")
 
     gl = gitlab.Gitlab(GITLAB_URL, private_token=PRIVATE_TOKEN)
-    group = gl.groups.get(GROUP_ID)
 
-    # List all projects in the group
-    print(f"Getting projects for {group.name} ({group.id})")
-    group_projects = group.projects.list(get_all=True, archived=False)
+    rows, count = manage_projects_in_group(
+        gl,
+        GROUP_ID,
+        fix=args.fix,
+        limit=args.limit,
+        recurse=args.recurse_subprojects,
+        count=0,
+    )
 
     table = PrettyTable()
     table.align = "l"
-    table.field_names = [
-        "project",
-        "default_branch",
-        "squash_option",
-        "prevent_secrets",
-        "remove_source_branch_after_merge",
-        "only_allow_merge_if_pipeline_succeed",
-        "merge_method",
-        "protected branches",
-        "merge_access_levels",
-        # ""
-    ]
+    table.field_names = rows[0].keys()
 
-    count = 0
-    for group_project in group_projects:
-
-        project_details = gl.projects.get(group_project.id)
-
-        try:
-            row = extract_project_details(project_details, args.fix)
-            table.add_row(row)
-        except Exception as e:
-            # log and continue
-            logging.exception(e)
-
-        if count == args.limit:
-            print(f"Limit of {args.limit} reached. Exiting")
-            break
-        count += 1
+    for row in rows:
+        table.add_row(row.values())
 
     print(table)
 
