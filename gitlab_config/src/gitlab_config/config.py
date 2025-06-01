ff76b62982518732392e19340465025a36da1e45
Simon Merrick <s.m3rrick@gmail.com>
2025-06-02 01:00:16 +1200

    tidy

--- gitlab_config/src/gitlab_config/config.py
+++ gitlab_config/src/gitlab_config/config.py
@@ -14,7 +14,4 @@ def load_yaml_config(filename: Path = "config.yaml"):
 
 
 CONFIG = load_yaml_config()
-# LOG_LEVEL = os.environ.get("LOG_LEVEL", "WARNING").upper()
-# GITLAB_CONFIG_LOG_LEVEL = os.environ.get("GITLAB_CONFIG_LOG_LEVEL", LOG_LEVEL)
-# GITLAB_URL = os.environ.get("GITLAB_URL", "https://gitlab.com")
 GITLAB_TOKEN = os.environ.get("GITLAB_TOKEN")
